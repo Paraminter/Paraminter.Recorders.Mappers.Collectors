ddc0d6e576a65808cdc3caa9a4e8c5fb966d0e62
ErikWe <erikwestberg7@gmail.com>
2024-03-10 15:01:45 +0100

    Removed references to parameter-keys

--- src/Attribinter.Mappers.Collectors.Abstractions/IParameterMapperFactory.cs
+++ src/Attribinter.Mappers.Collectors.Abstractions/IParameterMapperFactory.cs
@@ -2,7 +2,7 @@
 
 using System.Collections.Generic;
 
-/// <summary>Handles creation of <see cref="IParameterMapper{TParameter, TRecord, TData}"/> using <see cref="IParameterMappingRegistrator{TKey, TRecord, TData}"/>.</summary>
+/// <summary>Handles creation of <see cref="IParameterMapper{TParameter, TRecord, TData}"/> using <see cref="IParameterMappingRegistrator{TParameter, TRecord, TData}"/>.</summary>
 public interface IParameterMapperFactory
 {
     /// <summary>Creates a <see cref="IParameterMapper{TParameter, TRecord, TData}"/>, mapping parameters to recorders.</summary>
--- src/Attribinter.Mappers.Collectors.Abstractions/IParameterMappingRepositoryFactory.cs
+++ src/Attribinter.Mappers.Collectors.Abstractions/IParameterMappingRepositoryFactory.cs
@@ -12,13 +12,13 @@ public interface IParameterMappingRepositoryFactory
     public abstract IParameterMappingRepositoryFactory<TParameter> ForParameter<TParameter>(IEqualityComparer<TParameter> parameterComparer);
 }
 
-/// <summary>Handles creation of <see cref="IParameterMappingRepository{TKey, TRecord, TData}"/>.</summary>
-/// <typeparam name="TParameter">The type of the key used to identify the mapped parameters.</typeparam>
+/// <summary>Handles creation of <see cref="IParameterMappingRepository{TParameter, TRecord, TData}"/>.</summary>
+/// <typeparam name="TParameter">The type of the mapped parameters.</typeparam>
 public interface IParameterMappingRepositoryFactory<TParameter>
 {
-    /// <summary>Creates a <see cref="IParameterMappingRepository{TKey, TRecord, TData}"/>.</summary>
+    /// <summary>Creates a <see cref="IParameterMappingRepository{TParameter, TRecord, TData}"/>.</summary>
     /// <typeparam name="TRecord">The type of the data record to which the mapped recorders record data.</typeparam>
     /// <typeparam name="TData">The type of the data recorded by the mapped recorders.</typeparam>
-    /// <returns>The created <see cref="IParameterMappingRepository{TKey, TRecord, TData}"/>.</returns>
+    /// <returns>The created <see cref="IParameterMappingRepository{TParameter, TRecord, TData}"/>.</returns>
     public abstract IParameterMappingRepository<TParameter, TRecord, TData> Create<TRecord, TData>();
 }
